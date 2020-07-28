4b8654e2a178ed21fed65fbe366ca4e674186cf1
Rohan Varma <rvarm1@fb.com>
2020-07-27 18:10:16 -0700

    Enable test_distributed to work with spawn mode
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/41769
    
    Currently the tests in `test_distributed` only work with the `fork` mode multiprocessing, this PR introduces support for `spawn` mode multiprocessing as well (while keeping the `fork` mode intact).
    
    Motivations for the change:
    1) Spawn multiprocessing is the default on MacOS, so it better emulates how MacOS users would use distributed
    2) With python 3.8+, spawn is the default on linux, so we should have test coverage for this
    3) PT multiprocessing suggests using spawn/forkserver over fork, for sharing cuda tensors: https://pytorch.org/docs/stable/multiprocessing.html
    4) Spawn is better supported with respect to certain sanitizers such as TSAN, so adding this sanitizer coverage may help us uncover issues.
    
    Differential Revision: [D22408023](https://our.internmc.facebook.com/intern/diff/D22408023/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D22408023/)!
    ghstack-source-id: 108625084

--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -174,6 +174,22 @@ def simple_sparse_reduce_tests(rank, world_size, num_inputs=1):
         ]
     ]
 
+tmp_dir = None
+def initialize_temp_directories(init_method=None):
+    global tmp_dir
+    tmp_dir = tempfile.TemporaryDirectory()
+    os.environ["TEMP_DIR"] = tmp_dir.name
+    os.mkdir(os.path.join(tmp_dir.name, "barrier"))
+    os.mkdir(os.path.join(tmp_dir.name, "test_dir"))
+    init_dir_path = os.path.join(tmp_dir.name, "init_dir")
+    os.mkdir(init_dir_path)
+    # Set init method if specified.
+    if init_method is not None:
+        os.environ["INIT_METHOD"] = init_method
+    else:
+        os.environ["INIT_METHOD"] = "file://" + os.path.join(
+            init_dir_path, "shared_init_file"
+        )
 
 # [How does MultiProcessTestCase work?]
 # Each MultiProcessTestCase instance uses 1 + `world_size()` processes, by
@@ -249,6 +265,7 @@ class MultiProcessTestCase(TestCase):
                 args=(rank, self._current_test_name(), self.file_name))
             process.start()
             self.processes.append(process)
+
 
     def _fork_processes(self):
         proc = torch.multiprocessing.get_context("fork").Process
